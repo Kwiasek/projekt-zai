7a9ad7c321ed76293beda4714b11595239246d06
Kwiasek <kwiasekkwiasek@gmail.com>
2026-03-04 16:45:16 +0100

    Initial commit

---
+++ backend/src/main/java/com/kwiasek/sklep_backend/SklepBackendApplication.java
@@ -0,0 +1,13 @@
+package com.kwiasek.sklep_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SklepBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SklepBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/kwiasek/sklep_backend/SklepBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kwiasek.sklep_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SklepBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
